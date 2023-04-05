da30bf399f14c2dc2a23d4778d5e4a1e2f3c93f9
geri.kadiu <geri.kadiu@comm-it.it>
2023-04-05 13:53:25 +0200

    404 page

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+export default function PageNotFound() {
+  return (
+    <h1
+      style={{
+        flexDirection: "column",
+        alignItems: "center",
+        display: "flex",
+      }}
+    >
+      PageNotFound
+    </h1>
+  );
+}
