c837318a3c1ce95e1f55a2178f17c2eea0a2a97d
geri.kadiu <geri.kadiu@comm-it.it>
2023-04-05 13:15:11 +0200

    getStaticProps

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,13 +1,58 @@
 import MeetupDetail from "@/components/meetups/MeetupDetail";
+import { MongoClient, ObjectId } from "mongodb";
 import { Fragment } from "react";
 
-export default function MeetupDetails() {
+export default function MeetupDetails(props) {
+  console.log(props);
   return (
     <MeetupDetail
-      image="https://www.kdfc.com/wp-content/uploads/2020/07/road-trip-classical-california-summer-coast-iStock-1145661962.jpg"
-      title="First Meetup"
-      address="Some street, 5, Some city"
-      description="This is a first meetup"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
+}
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://gajdeg:AYeZPFgjvr6Olh2u@cluster0.6asz3y8.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+  client.close();
+  return {
+    fallback: false,
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  const client = await MongoClient.connect(
+    "mongodb+srv://gajdeg:AYeZPFgjvr6Olh2u@cluster0.6asz3y8.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: new ObjectId(meetupId),
+  });
+  console.log(selectedMeetup);
+  client.close();
+
+  return {
+    props: {
+      meetupData: {
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
+      },
+    },
+  };
 }
